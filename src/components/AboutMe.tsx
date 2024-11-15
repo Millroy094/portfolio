b1bd0d80474f7a65dd755c80ee8b1185100b5246
Millroy094 <58091953+Millroy094@users.noreply.github.com>
2024-11-15 00:07:33 +0000

    chore: progress

--- src/components/AboutMe.tsx
+++ src/components/AboutMe.tsx
@@ -1,75 +1,75 @@
-import { Box, Typography } from "@mui/material";
-import { Zoom } from "react-awesome-reveal";
-import StackIcon from "tech-stack-icons";
+import { Grid2 as Grid, Typography } from '@mui/material';
+import { Zoom } from 'react-awesome-reveal';
+import Lottie from 'react-lottie';
+import AboutMeLottie from '../lotties/about-me.json';
 
 function AboutMe() {
-  return (
-    <Box
-      sx={{
-        padding: "20px",
-        display: "flex",
-        flexDirection: "column",
-      }}
-    >
-      <Zoom cascade>
-        <Box
-          sx={{
-            display: "flex",
-            justifyContent: "center",
-            gap: "12px",
-          }}
-        >
-          <Typography
-            variant="overline"
-            fontWeight="bold"
-            fontSize={35}
-            sx={{
-              display: "inline-block",
-            }}
-          >
-            About
-          </Typography>
-          <Typography
-            variant="overline"
-            fontWeight="bold"
-            fontSize={35}
-            sx={{
-              color: "#d32f2f",
-              display: "inline-block",
-            }}
-          >
-            Me
-          </Typography>
-        </Box>
+  const defaultOptions = {
+    loop: true,
+    autoplay: true,
+    animationData: AboutMeLottie,
+    rendererSettings: {
+      preserveAspectRatio: 'xMidYMid slice',
+    },
+  };
 
-        <Box
-          sx={{
-            display: "flex",
-            justifyContent: "center",
-            gap: "12px",
-          }}
+  return (
+    <Grid container direction='column' sx={{ marginBottom: '60px' }}>
+      <Zoom cascade>
+        <Grid
+          container
+          spacing={2}
+          justifyItems='center'
+          sx={{ padding: '0 40px' }}
         >
-          <Box
-            sx={{
-              display: "flex",
-              justifyContent: "center",
-              gap: "12px",
-              width: "50%",
-            }}
-          >
-            <Typography variant="body1">
-              I've been a developer for about 6+ years, I've climbed my way up
-              from a customer service advisor to a support engineer to a
-              software engineer and since then I've been on this amazing journey
-              where there is always something to learn. I am well versed with
-              not only the dev world but everything outside and everything in
-              between. Well through my whole career my only goal has been to
-              learn, to do the right thing, and be better than my past self.
-            </Typography>
-          </Box>
-        </Box>
+          <Grid container size={4}>
+            <Lottie options={defaultOptions} width={400} height={400} />
+          </Grid>
+          <Grid container size={8} alignContent='center'>
+            <Grid container justifyContent='start' spacing={1}>
+              <Typography
+                variant='overline'
+                fontWeight='bold'
+                fontSize={35}
+                sx={{
+                  display: 'inline-block',
+                }}
+              >
+                About
+              </Typography>
+              <Typography
+                variant='overline'
+                fontWeight='bold'
+                fontSize={35}
+                sx={{
+                  color: '#d32f2f',
+                  display: 'inline-block',
+                }}
+              >
+                Me
+              </Typography>
+            </Grid>
+            <Grid container>
+              <Typography variant='body1' fontSize='20px'>
+                I've been a developer for about 6+ years, I've climbed my way up
+                from a customer service advisor to a support engineer to a
+                software engineer and since then I've been on this amazing
+                journey where there is always something to learn. I am well
+                versed with not only the dev world but everything outside and
+                everything in between. Well through my whole career my only goal
+                has been to learn, to do the right thing, and be better than my
+                past self.
+              </Typography>
+              <Typography variant='body1' fontSize='20px'>
+                I've trained myself up as a DevOps and a QA engineer as much as
+                my fullstack experience because I feel as Dev you need to know
+                how important these roles are.
+              </Typography>
+            </Grid>
+          </Grid>
+        </Grid>
       </Zoom>
-    </Box>
+    </Grid>
   );
 }
 
--- src/components/Skills.tsx
+++ src/components/Skills.tsx
@@ -1,74 +1,92 @@
-import { Box, Typography } from "@mui/material";
-import { Zoom } from "react-awesome-reveal";
-import StackIcon from "tech-stack-icons";
+import { Grid2 as Grid, Typography } from '@mui/material';
+import { Zoom } from 'react-awesome-reveal';
+import StackIcon from 'tech-stack-icons';
+import {
+  SiDotnet,
+  SiExpress,
+  SiGithubactions,
+  SiHelm,
+  SiTerraform,
+} from '@icons-pack/react-simple-icons';
+import Lottie from 'react-lottie';
+import SkillsLottie from '../lotties/skills.json';
 
 function Skills() {
-  return (
-    <Box
-      sx={{
-        padding: "20px",
-        display: "flex",
-        flexDirection: "column",
-      }}
-    >
-      <Zoom cascade>
-        <Box
-          sx={{
-            display: "flex",
-            justifyContent: "center",
-            gap: "12px",
-          }}
-        >
-          <Typography
-            variant="overline"
-            fontWeight="bold"
-            fontSize={35}
-            sx={{
-              display: "inline-block",
-            }}
-          >
-            Technology &
-          </Typography>
-          <Typography
-            variant="overline"
-            fontWeight="bold"
-            fontSize={35}
-            sx={{
-              color: "#d32f2f",
-              display: "inline-block",
-            }}
-          >
-            Skills
-          </Typography>
-        </Box>
+  const skills = [
+    <StackIcon key='reactjs' name='reactjs' style={{ width: '40px' }} />,
+    <StackIcon key='redux' name='redux' style={{ width: '40px' }} />,
+    <StackIcon key='nodejs' name='nodejs' style={{ width: '40px' }} />,
+    <StackIcon key='typescript' name='typescript' style={{ width: '40px' }} />,
+    <SiExpress key='express' size={40} color='#ffffff' />,
+    <StackIcon key='graphql' name='graphql' style={{ width: '40px' }} />,
+    <StackIcon key='go' name='go' style={{ width: '40px' }} />,
+    <StackIcon key='java' name='java' style={{ width: '40px' }} />,
+    <StackIcon key='php' name='php' style={{ width: '40px' }} />,
+    <StackIcon key='laravel' name='laravel' style={{ width: '40px' }} />,
+    <StackIcon key='csharp' name='csharp' style={{ width: '40px' }} />,
+    <SiDotnet key='dotnet' size={40} color='#2088FF' />,
+    <StackIcon key='jest' name='jest' style={{ width: '40px' }} />,
+    <StackIcon key='cypress' name='cypress' style={{ width: '40px' }} />,
+    <StackIcon key='playwright' name='playwright' style={{ width: '40px' }} />,
+    <StackIcon key='mongodb' name='mongodb' style={{ width: '40px' }} />,
+    <StackIcon key='redis' name='redis' style={{ width: '40px' }} />,
+    <StackIcon key='mysql' name='mysql' style={{ width: '40px' }} />,
+    <StackIcon key='postgresql' name='postgresql' style={{ width: '40px' }} />,
+    <StackIcon key='docker' name='docker' style={{ width: '40px' }} />,
+    <StackIcon key='kubernetes' name='kubernetes' style={{ width: '40px' }} />,
+    <SiHelm key='helm' size={40} color='#0F1689' />,
+    <SiTerraform key='terraform' size={40} color='#844FBA' />,
+    <SiGithubactions key='githubactions' size={40} color='#2088FF' />,
+    <StackIcon key='aws' name='aws' style={{ width: '40px' }} />,
+  ];
 
-        <Box
-          sx={{
-            display: "flex",
-            justifyContent: "center",
-            gap: "12px",
-            padding: "30px 240px 0",
-          }}
-        >
-          <StackIcon name="reactjs" />
-          <StackIcon name="redux" />
-          <StackIcon name="nodejs" />
-          <StackIcon name="typescript" />
-          <StackIcon name="graphql" />
-          <StackIcon name="go" />
-          <StackIcon name="php" />
-          <StackIcon name="laravel" />
-          <StackIcon name="jest" />
-          <StackIcon name="playwright" />
-          <StackIcon name="cypress" />
-          <StackIcon name="mongodb" />
-          <StackIcon name="mysql" />
-          <StackIcon name="postgresql" />
-          <StackIcon name="docker" />
-          <StackIcon name="kubernetes" />
-        </Box>
-      </Zoom>
-    </Box>
+  const defaultOptions = {
+    loop: true,
+    autoplay: true,
+    animationData: SkillsLottie,
+    rendererSettings: {
+      preserveAspectRatio: 'xMidYMid slice',
+    },
+  };
+
+  return (
+    <Zoom cascade>
+      <Grid container justifyContent='center'>
+        <Grid container size={3}>
+          <Lottie options={defaultOptions} height={400} width={400} />
+        </Grid>
+        <Grid container size={9} sx={{ padding: '40px' }}>
+          <Grid container spacing={2}>
+            <Typography
+              variant='overline'
+              fontWeight='bold'
+              fontSize={35}
+              sx={{
+                display: 'inline-block',
+              }}
+            >
+              Technology &
+            </Typography>
+            <Typography
+              variant='overline'
+              fontWeight='bold'
+              fontSize={35}
+              sx={{
+                color: '#d32f2f',
+                display: 'inline-block',
+              }}
+            >
+              Skills
+            </Typography>
+          </Grid>
+          <Grid container spacing={2}>
+            {skills?.map((skill: JSX.Element) => (
+              <Grid key={skill.key}>{skill}</Grid>
+            ))}
+          </Grid>
+        </Grid>
+      </Grid>
+    </Zoom>
   );
 }
 
