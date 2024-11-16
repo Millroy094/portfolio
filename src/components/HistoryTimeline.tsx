f23115925291afcb2a9ac41bb454b661767a1053
Millroy094 <58091953+Millroy094@users.noreply.github.com>
2024-11-16 02:19:49 +0000

    feat: tweaks

--- src/components/HistoryTimeline.tsx
+++ src/components/HistoryTimeline.tsx
@@ -1,4 +1,4 @@
-import { ReactElement } from "react";
+import { ReactElement } from 'react';
 import {
   Timeline,
   TimelineConnector,
@@ -7,9 +7,10 @@ import {
   TimelineItem,
   TimelineOppositeContent,
   TimelineSeparator,
-} from "@mui/lab";
-import { Grid2 as Grid, Typography } from "@mui/material";
-import GrowOnHover from "../hoc/GrowOnHover";
+} from '@mui/lab';
+import { Grid2 as Grid, Typography } from '@mui/material';
+import { timelineContentClasses } from '@mui/lab/TimelineContent';
+import GrowOnHover from '../hoc/GrowOnHover';
 
 interface ITimelineItem {
   year: number;
@@ -28,28 +29,38 @@ function HistoryTimeline(props: Readonly<IHistoryTimelineProps>) {
   return (
     <Grid
       container
-      direction="column"
-      alignItems="center"
+      direction='column'
+      alignItems='center'
       size={{ xs: 12, sm: 12, md: 6, lg: 4 }}
     >
       <Typography
-        variant="overline"
-        fontWeight="bold"
+        variant='overline'
+        fontWeight='bold'
         fontSize={{ xs: 20, sm: 20, md: 35, lg: 35 }}
       >
         {title}
       </Typography>
-      <Timeline sx={{ width: "100%" }}>
+      <Timeline
+        sx={{
+          [`& .${timelineContentClasses.root}`]: {
+            justifyContent: 'center',
+          },
+        }}
+      >
         {timeline?.map((t: ITimelineItem, index: number) => (
           <GrowOnHover key={t.title}>
             <TimelineItem>
-              <TimelineOppositeContent>{t.year}</TimelineOppositeContent>
+              <TimelineOppositeContent
+                sx={{ display: 'flex', flex: 0, alignItems: 'center' }}
+              >
+                {t.year}
+              </TimelineOppositeContent>
               <TimelineSeparator>
-                <TimelineDot color="error">{t.Icon}</TimelineDot>
+                <TimelineDot color='error'>{t.Icon}</TimelineDot>
                 {index !== timeline.length - 1 && <TimelineConnector />}
               </TimelineSeparator>
-              <TimelineContent sx={{ flex: 2 }}>
-                <Typography variant="h6" component="span">
+              <TimelineContent sx={{ flex: 1 }}>
+                <Typography variant='h6' component='span'>
                   {t.title}
                 </Typography>
                 <Typography>{t.subTitle}</Typography>
