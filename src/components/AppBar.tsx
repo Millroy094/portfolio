fd54d29c580335a6c9e2415a87fa87f34b87259d
Millroy Fernandes <132992164+mfernandes-alcumus@users.noreply.github.com>
2024-11-18 12:58:04 +0000

    feat: added a menu

---
+++ src/components/AppBar.tsx
@@ -0,0 +1,153 @@
+import * as React from "react";
+import AppBar from "@mui/material/AppBar";
+import Box from "@mui/material/Box";
+import Toolbar from "@mui/material/Toolbar";
+import IconButton from "@mui/material/IconButton";
+import Typography from "@mui/material/Typography";
+import Menu from "@mui/material/Menu";
+import MenuIcon from "@mui/icons-material/Menu";
+import Container from "@mui/material/Container";
+import Button from "@mui/material/Button";
+import MenuItem from "@mui/material/MenuItem";
+import Logo from "../assets/Informic.svg?react";
+import { Link } from "react-scroll";
+
+const pages = ["About Me", "Skills", "Education And Experience", "Projects"];
+
+function PortfolioAppBar() {
+  const [anchorElNav, setAnchorElNav] = React.useState<null | HTMLElement>(
+    null
+  );
+
+  const handleOpenNavMenu = (event: React.MouseEvent<HTMLElement>) => {
+    setAnchorElNav(event.currentTarget);
+  };
+
+  const handleCloseNavMenu = () => {
+    setAnchorElNav(null);
+  };
+
+  return (
+    <AppBar position="static">
+      <Container maxWidth="xl" sx={{ padding: { xs: "0" } }}>
+        <Toolbar disableGutters>
+          <Box
+            sx={{
+              display: { xs: "none", md: "flex" },
+              background: "white",
+              width: "50px",
+              height: "50px",
+              overflow: "hidden",
+              justifyContent: "center",
+              alignItems: "center",
+              marginRight: "20px",
+            }}
+          >
+            <Link
+              smooth
+              duration={1000}
+              delay={100}
+              offset={0}
+              to="introduction"
+            >
+              <Logo />
+            </Link>
+          </Box>
+
+          <Box
+            sx={{
+              flexGrow: 1,
+              display: { xs: "flex", md: "none" },
+              background: "white",
+              opacity: 0.8,
+            }}
+          >
+            <IconButton
+              size="large"
+              aria-label="account of current user"
+              aria-controls="menu-appbar"
+              aria-haspopup="true"
+              onClick={handleOpenNavMenu}
+            >
+              <MenuIcon />
+            </IconButton>
+            <Menu
+              id="menu-appbar"
+              anchorEl={anchorElNav}
+              anchorOrigin={{
+                vertical: "bottom",
+                horizontal: "left",
+              }}
+              keepMounted
+              transformOrigin={{
+                vertical: "top",
+                horizontal: "left",
+              }}
+              open={Boolean(anchorElNav)}
+              onClose={handleCloseNavMenu}
+              sx={{
+                display: { xs: "block", md: "none" },
+                opacity: 0.8,
+              }}
+            >
+              {pages.map((page) => (
+                <MenuItem key={page} onClick={handleCloseNavMenu}>
+                  <Link
+                    smooth
+                    duration={1000}
+                    delay={100}
+                    to={page.toLowerCase().replace(/\s/g, "")}
+                  >
+                    <Typography
+                      variant="caption"
+                      sx={{ textAlign: "center", color: "black" }}
+                    >
+                      {page}
+                    </Typography>
+                  </Link>
+                </MenuItem>
+              ))}
+            </Menu>
+            <Box
+              sx={{
+                display: "flex",
+                background: "white",
+                width: "50px",
+                height: "50px",
+                overflow: "hidden",
+                justifyContent: "center",
+                alignItems: "center",
+                marginRight: "20px",
+              }}
+            >
+              <Link smooth duration={1000} delay={100} to="introduction">
+                <Logo />
+              </Link>
+            </Box>
+          </Box>
+          <Box sx={{ flexGrow: 1, display: { xs: "none", md: "flex" } }}>
+            {pages.map((page) => (
+              <Button
+                key={page}
+                onClick={handleCloseNavMenu}
+                sx={{ my: 2, color: "white", display: "block" }}
+              >
+                <Link
+                  smooth
+                  duration={1000}
+                  delay={100}
+                  to={page.toLowerCase().replace(/\s/g, "")}
+                >
+                  <Typography variant="button" fontWeight="bold" color="white">
+                    {page}
+                  </Typography>
+                </Link>
+              </Button>
+            ))}
+          </Box>
+        </Toolbar>
+      </Container>
+    </AppBar>
+  );
+}
+export default PortfolioAppBar;
---
+++ src/hoc/GrowAndShowLabelOnHover.tsx
@@ -0,0 +1,48 @@
+import { motion } from "motion/react";
+import { ReactElement } from "react";
+import GrowOnHover from "./GrowOnHover";
+import { Typography } from "@mui/material";
+
+interface IGrowAndShowLabelOnHoverProps {
+  children: ReactElement;
+  label: string;
+  scale?: number;
+}
+
+const labelMotion = {
+  rest: { opacity: 0, ease: "easeOut", duration: 0.2, type: "tween" },
+  hover: {
+    opacity: 1,
+    transition: {
+      duration: 0.4,
+      type: "tween",
+      ease: "easeIn",
+    },
+  },
+};
+
+function GrowAndShowLabelOnHover(
+  props: Readonly<IGrowAndShowLabelOnHoverProps>
+) {
+  const { children, label, scale } = props;
+  return (
+    <motion.div
+      initial="rest"
+      whileHover="hover"
+      animate="rest"
+      style={{
+        display: "flex",
+        flexDirection: "column",
+        alignItems: "center",
+        width: "100%",
+      }}
+    >
+      <GrowOnHover scale={scale}>{children}</GrowOnHover>
+      <motion.div variants={labelMotion}>
+        <Typography variant="caption">{label}</Typography>
+      </motion.div>
+    </motion.div>
+  );
+}
+
+export default GrowAndShowLabelOnHover;
