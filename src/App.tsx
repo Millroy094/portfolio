c3f6f32b1913790f02ed0247632625a97296bf43
Millroy094 <58091953+Millroy094@users.noreply.github.com>
2024-11-18 21:53:23 +0000

    feat: enhanced scroll up

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,11 @@
-import { useEffect } from 'react';
+import { useEffect, useState } from 'react';
 import {
   Element,
   Events,
   animateScroll as scroll,
   scrollSpy,
 } from 'react-scroll';
+import { AnimatePresence, motion } from 'motion/react';
 import AboutMe from './components/AboutMe';
 import BackgroundParticles from './components/BackgroundParticles';
 import EducationAndExperience from './components/EducationAndExpierence';
@@ -13,8 +14,20 @@ import Projects from './components/Projects';
 import Skills from './components/Skills';
 import { Fab, Grid2 as Grid } from '@mui/material';
 import { SwipeUp } from '@mui/icons-material';
+import useWindowDimensions from './hooks/useWindowDimensions';
 
 function App() {
+  const { height } = useWindowDimensions();
+
+  const [scrollOffset, setScrollOffset] = useState(0);
+
+  useEffect(() => {
+    const onScroll = () => setScrollOffset(window.scrollY);
+    window.removeEventListener('scroll', onScroll);
+    window.addEventListener('scroll', onScroll, { passive: true });
+    return () => window.removeEventListener('scroll', onScroll);
+  }, []);
+
   useEffect(() => {
     scrollSpy.update();
 
@@ -46,29 +59,46 @@ function App() {
       <Element name='projects'>
         <Projects />
       </Element>
-      <Grid
-        container
-        justifyContent='end'
-        p={1}
-        sx={{
-          margin: 0,
-          top: 'auto',
-          right: 20,
-          bottom: 20,
-          left: 'auto',
-          position: 'fixed',
-          zIndex: 100,
-        }}
-      >
-        <Fab
-          color='error'
-          aria-label='add'
-          onClick={scrollToTop}
-          sx={{ '&.MuiFab-root:focus': { outline: 'none' } }}
-        >
-          <SwipeUp />
-        </Fab>
-      </Grid>
+      <AnimatePresence AnimatePresence mode='wait'>
+        {scrollOffset > height && (
+          <Grid
+            container
+            justifyContent='end'
+            p={1}
+            sx={{
+              margin: 0,
+              top: 'auto',
+              right: 20,
+              bottom: 20,
+              left: 'auto',
+              position: 'fixed',
+              zIndex: 100,
+            }}
+          >
+            <motion.div
+              key={location.pathname}
+              initial={{ opacity: 0, y: '15%' }}
+              animate={{ opacity: 1, y: 0 }}
+              exit={{ opacity: 0 }}
+              transition={{
+                ease: 'linear',
+                duration: 0.5,
+              }}
+            >
+              <Fab
+                color='error'
+                aria-label='add'
+                onClick={scrollToTop}
+                sx={{
+                  '&.MuiFab-root:focus': { outline: 'none' },
+                }}
+              >
+                <SwipeUp />
+              </Fab>
+            </motion.div>
+          </Grid>
+        )}
+      </AnimatePresence>
     </>
   );
 }
