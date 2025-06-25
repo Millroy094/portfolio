adf64ed8fcdcb576e2fb2c1441db818af11f7bd9
Millroy094 <58091953+Millroy094@users.noreply.github.com>
2025-06-25 19:21:38 +0100

    chore: more skills

--- src/hoc/GrowAndShowLabelOnHover.tsx
+++ src/hoc/GrowAndShowLabelOnHover.tsx
@@ -1,8 +1,8 @@
-import { motion } from "motion/react";
-import { ReactElement } from "react";
-import GrowOnHover from "./GrowOnHover";
-import { Typography } from "@mui/material";
-import useMediaQuery from "@mui/material/useMediaQuery";
+import { motion } from 'motion/react';
+import { ReactElement } from 'react';
+import GrowOnHover from './GrowOnHover';
+import { Typography } from '@mui/material';
+import useMediaQuery from '@mui/material/useMediaQuery';
 
 interface IGrowAndShowLabelOnHoverProps {
   children: ReactElement;
@@ -11,43 +11,43 @@ interface IGrowAndShowLabelOnHoverProps {
 }
 
 const labelMotion = {
-  rest: { opacity: 0, ease: "easeOut", duration: 0.2, type: "tween" },
+  rest: { opacity: 0, ease: 'easeOut', duration: 0.2, type: 'tween' },
   hover: {
     opacity: 1,
     transition: {
       duration: 0.4,
-      type: "tween",
-      ease: "easeIn",
+      type: 'tween',
+      ease: 'easeIn',
     },
   },
 };
 
 function GrowAndShowLabelOnHover(
-  props: Readonly<IGrowAndShowLabelOnHoverProps>
+  props: Readonly<IGrowAndShowLabelOnHoverProps>,
 ) {
   const { children, label, scale } = props;
-  const isBelowMidSize = useMediaQuery("(max-width:959px)");
+  const isBelowMidSize = useMediaQuery('(max-width:959px)');
 
   return (
     <motion.div
-      initial="rest"
-      whileHover="hover"
-      animate="rest"
+      initial='rest'
+      whileHover='hover'
+      animate='rest'
       style={{
-        display: "flex",
-        flexDirection: "column",
-        alignItems: "center",
-        width: "100%",
+        display: 'flex',
+        flexDirection: 'column',
+        alignItems: 'center',
+        width: '100%',
       }}
     >
       <GrowOnHover scale={scale}>{children}</GrowOnHover>
       {isBelowMidSize ? (
-        <Typography variant="caption" textAlign="center">
+        <Typography variant='caption' textAlign='center'>
           {label}
         </Typography>
       ) : (
-        <motion.div variants={labelMotion}>
-          <Typography variant="caption" textAlign="center">
+        <motion.div variants={labelMotion} style={{ display: 'flex' }}>
+          <Typography variant='caption' textAlign='center'>
             {label}
           </Typography>
         </motion.div>
