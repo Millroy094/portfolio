7b7e73750ebf261a899c7126fac3f89c56dd6c8e
Millroy <58091953+Millroy094@users.noreply.github.com>
2026-02-04 13:46:03 +0000

    fix: removed service role aws built in policy

--- infra/main.tf
+++ infra/main.tf
@@ -1,7 +1,6 @@
 resource "aws_amplify_app" "portfolio" {
   name                 = "portfolio"
   repository           = "https://github.com/Millroy094/portfolio"
-  iam_service_role_arn = aws_iam_role.amplify_service_role.arn
   access_token         = var.github_token
   build_spec           = file("${path.module}/amplify.yml")
 }
