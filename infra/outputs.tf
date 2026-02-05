c42cfaa7bc09a42fbaaff311b832d8965b7097b5
Millroy <58091953+Millroy094@users.noreply.github.com>
2026-02-05 12:16:00 +0000

    refactor: deployment style

--- infra/outputs.tf
+++ infra/outputs.tf
@@ -2,3 +2,7 @@ output "amplify_app_url" {
   value       = format("https://%s.%s.amplifyapp.com", aws_amplify_branch.deployment_branch.branch_name, aws_amplify_app.portfolio.id)
   description = "Public URL for the deployed Amplify app"
 }
+
+output "amplify_app_id" {
+  value = aws_amplify_app.portfolio.id
+}
