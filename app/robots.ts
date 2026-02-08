0484cdbbef91aa6e0519cba791342fe62cfbb855
Millroy <58091953+Millroy094@users.noreply.github.com>
2026-02-08 20:46:58 +0000

    Update robots.ts

--- app/robots.ts
+++ app/robots.ts
@@ -7,6 +7,6 @@ export default function robots(): MetadataRoute.Robots {
       allow: '/',
       disallow: ['/admin/', '/login/'],
     },
-    sitemap: 'https://www.millroyfernandes.com',
+    sitemap: 'https://www.millroyfernandes.com/sitemap.xml',
   }
 }
