85d46669c586648912e32d64c3970e899755c46e
Millroy <58091953+Millroy094@users.noreply.github.com>
2026-02-12 12:11:43 +0000

    feat: prettier setup

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -2,7 +2,7 @@ import { defineConfig, globalIgnores } from "eslint/config";
 import nextVitals from "eslint-config-next/core-web-vitals";
 import nextTs from "eslint-config-next/typescript";
 
-const eslintConfig = defineConfig([
+export default defineConfig([
   ...nextVitals,
   ...nextTs,
   {
@@ -14,5 +14,3 @@ const eslintConfig = defineConfig([
   },
   globalIgnores([".next/**", "out/**", "build/**", "next-env.d.ts"]),
 ]);
-
-export default eslintConfig;
