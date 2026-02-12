7cd9cf951295c1f29c8a11dde185605fd50fe9dd
Millroy <58091953+Millroy094@users.noreply.github.com>
2026-02-12 12:14:46 +0000

    feat: prettier setup

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,16 +1,20 @@
 import { defineConfig, globalIgnores } from "eslint/config";
 import nextVitals from "eslint-config-next/core-web-vitals";
 import nextTs from "eslint-config-next/typescript";
+import prettierPlugin from "eslint-plugin-prettier";
 
 export default defineConfig([
   ...nextVitals,
   ...nextTs,
+
   {
-    plugins: ["prettier"],
-    extends: ["plugin:prettier/recommended"],
+    plugins: {
+      prettier: prettierPlugin,
+    },
     rules: {
       "prettier/prettier": "warn",
     },
   },
-  globalIgnores([".next/**", "out/**", "build/**", "next-env.d.ts"]),
+
+  globalIgnores([".next/**", "out/**", "build/**", "next-env.d.ts", ".amplify/**"]),
 ]);
