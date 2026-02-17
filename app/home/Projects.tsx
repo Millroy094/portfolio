47d4a353e140404d9750e9aa0462ead4f563c2dc
Millroy <58091953+Millroy094@users.noreply.github.com>
2026-02-17 19:29:04 +0000

    fix: styling issues

--- app/home/Projects.tsx
+++ app/home/Projects.tsx
@@ -43,7 +43,7 @@ export default function Projects() {
 
       <div
         className="
-          grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-4 gap-4
+          grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4 gap-4
         "
       >
         {data.projects?.map((project) => (
