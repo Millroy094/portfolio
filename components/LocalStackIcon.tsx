94fec96e6d496d0631bbe64d5a13fc6d472e06cf
Millroy <58091953+Millroy094@users.noreply.github.com>
2026-05-20 14:05:38 +0100

    fix: LocalStackIcon.tsx change

--- components/LocalStackIcon.tsx
+++ components/LocalStackIcon.tsx
@@ -11,36 +11,20 @@ type Props = {
 const LocalstackIcon: React.FC<Props> = ({ size = 40, style, className }) => {
   return (
     <svg
-      xmlns="http://www.w3.org/2000/svg"
       viewBox="0 0 120 120"
       width={size}
       height={size}
-      fill="none"
-      preserveAspectRatio="xMidYMid meet"
+      xmlns="http://www.w3.org/2000/svg"
       className={className}
       style={style}
     >
-      {/* Cloud shape */}
-      <path
-        d="M30 70c-8 0-14-6-14-14s6-14 14-14c1 0 2 0 3 .2C36 32 46 24 58 24c14 0 25 11 25 25h2
-           c9 0 16 7 16 16s-7 16-16 16H30z"
-        fill="#2d4552"
-      />
-
-      {/* Container cube */}
-      <g transform="translate(40,55)">
-        {/* Top */}
-        <polygon points="20,0 40,10 20,20 0,10" fill="#e53935" />
-
-        {/* Left */}
-        <polygon points="0,10 20,20 20,40 0,30" fill="#c62828" />
-
-        {/* Right */}
-        <polygon points="40,10 20,20 20,40 40,30" fill="#ef5350" />
+      <rect width="120" height="120" rx="20" fill="#0f172a" />
+      <g transform="translate(20,30)">
+        <rect x="0" y="0" width="80" height="12" rx="3" fill="#ef4444" />
+        <rect x="8" y="18" width="80" height="12" rx="3" fill="#f97316" />
+        <rect x="16" y="36" width="80" height="12" rx="3" fill="#22c55e" />
+        <rect x="24" y="54" width="80" height="12" rx="3" fill="#3b82f6" />
       </g>
-
-      {/* Bottom underline */}
-      <rect x="30" y="95" width="60" height="6" fill="#1d8d22" />
     </svg>
   );
 };
